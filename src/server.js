0884510d82fceb95bf99341889941fc0f1a6e0eb
Lorena Góes <goesloren@gmail.com>
2021-08-28 15:12:53 -0300

    fix change port

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 import app from './app';
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Ouvindo porta ${PORT}`))
